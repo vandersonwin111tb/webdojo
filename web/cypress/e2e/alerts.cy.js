6163dc80ff9a26c6ee93428d2a20193e1fd8a322
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-06-02 16:16:23 -0300

    Alertas em JavaScript - Interagindo com prompt: usando stub e window para simular entradas

---
+++ web/cypress/e2e/alerts.cy.js
@@ -0,0 +1,54 @@
+describe('Valkidações de alertas em JavaScripts', ()=> {
+    beforeEach(()=> {
+        cy.login()
+        cy.goTo('Alertas JS', 'JavaScript Alerts')
+    })
+
+    it('Deve validar a mensagem de alerta', () => {
+        cy.log('todo')
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Olá QA, eu sou uma Alert Box!')
+        })
+
+        cy.contains('button', 'Mostrar Alert').click()
+    })
+
+    it('Deve confirmar um diálogo e validar a resposta positiva', () => {
+        cy.on('window:confirm', (msg) => {
+            expect(msg).to.equal('Aperta um botão!')
+            return true // True simula o click no botão Cancelar
+        })
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Você clicou em Ok!')
+        })
+
+        cy.contains('button', 'Mostrar Confirm').click()
+
+    })
+
+    it('Deve cancelar um diálogo e validar a resposta negativa', ()=> {
+         cy.on('window:confirm', (msg) => {
+            expect(msg).to.equal('Aperta um botão!')
+            return false // True simula o click no botão Cancelar
+        })
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Você cancelou!')
+        })
+        
+        cy.contains('button', 'Mostrar Confirm').click()
+    })
+
+    it.only('Deve interagir com um prompt, inserir um texto e validar uma mensagem', () => {
+        cy.window().then((win) => {
+            cy.stub(win, 'prompt').returns('Fernando')
+        })
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Olá Fernando! Boas vindas ao WebDojo!')
+        })
+
+        cy.contains('button', 'Mostrar Prompt').click()
+    })
+})
